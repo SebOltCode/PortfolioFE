1e1378724ce5f7bc0183c5b05f11464e3fb9e877
unknown <sebastian-olthoff@gmx.de>
2025-03-14 22:22:03 +0100

    had to remove reactscroll for now

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-scroll";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
@@ -9,16 +8,21 @@ const Navbar = () => {
     setIsOpen(!isOpen);
   };
 
+  // Funktion zum Schließen des Menüs nach Klick
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 p-4 bg-gray-900 text-white flex justify-between items-center shadow-lg">
       <h1 className="text-2xl font-bold">Portfolio</h1>
-      <div className="md:hidden" onClick={toggleMenu}>
+      <div className="md:hidden cursor-pointer" onClick={toggleMenu}>
         {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
       </div>
       <ul
-        className={`flex space-x-4 md:flex ${
+        className={`flex md:flex ${
           isOpen ? "flex" : "hidden"
-        } flex-col md:flex-row absolute md:static top-16 left-0 w-full md:w-auto bg-gray-900 md:bg-transparent`}
+        } flex-col md:flex-row absolute md:static top-16 left-0 w-full md:w-auto bg-gray-900 md:bg-transparent p-4 md:p-0 space-y-4 md:space-y-0 md:space-x-6`}
       >
         <li className="hidden md:block">
           <a
@@ -29,65 +33,61 @@ const Navbar = () => {
             <img
               src="./assets/linkedin.png"
               alt="LinkedIn"
-              className="h-6 w-6 hover:text-gray-400"
+              className="h-6 w-6 hover:opacity-75 transition-opacity"
             />
           </a>
         </li>
+
         <li>
-          <Link
-            to="home"
-            smooth={true}
-            duration={800}
-            className="hover:text-gray-400 cursor-pointer"
-            onClick={toggleMenu}
+          <a
+            href="#home"
+            className="hover:text-teal-500 cursor-pointer text-left bg-transparent border-none text-white p-0 font-inherit no-underline"
+            onClick={closeMenu}
           >
             Home
-          </Link>
+          </a>
         </li>
+
         <li>
-          <Link
-            to="experience"
-            smooth={true}
-            duration={800}
-            className="hover:text-gray-400 cursor-pointer"
-            onClick={toggleMenu}
+          <a
+            href="#experience"
+            className="hover:text-teal-500 cursor-pointer text-left bg-transparent border-none text-white p-0 font-inherit no-underline"
+            onClick={closeMenu}
           >
             Experience
-          </Link>
+          </a>
         </li>
+
         <li>
-          <Link
-            to="projects"
-            smooth={true}
-            duration={800}
-            className="hover:text-gray-400 cursor-pointer"
-            onClick={toggleMenu}
+          <a
+            href="#projects"
+            className="hover:text-teal-500 cursor-pointer text-left bg-transparent border-none text-white p-0 font-inherit no-underline"
+            onClick={closeMenu}
           >
             Projects
-          </Link>
+          </a>
         </li>
+
         <li>
-          <Link
-            to="about"
-            smooth={true}
-            duration={800}
-            className="hover:text-gray-400 cursor-pointer"
-            onClick={toggleMenu}
+          <a
+            href="#about"
+            className="hover:text-teal-500 cursor-pointer text-left bg-transparent border-none text-white p-0 font-inherit no-underline"
+            onClick={closeMenu}
           >
             About
-          </Link>
+          </a>
         </li>
+
         <li>
-          <Link
-            to="contact"
-            smooth={true}
-            duration={800}
-            className="hover:text-gray-400 cursor-pointer"
-            onClick={toggleMenu}
+          <a
+            href="#contact"
+            className="hover:text-teal-500 cursor-pointer text-left bg-transparent border-none text-white p-0 font-inherit no-underline"
+            onClick={closeMenu}
           >
             Contact
-          </Link>
+          </a>
         </li>
+
         <li className="md:hidden">
           <a
             href="https://www.linkedin.com/in/sebastian-olthoff/"
